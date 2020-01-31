ba8c894b9e68f231aba7e49af9bd151afaddfcbb
erinalara <48671321+erinalara@users.noreply.github.com>
2020-01-30 20:55:52 -0800

    Update Card.java

--- Card.java
+++ Card.java
@@ -6,28 +6,42 @@ public class Card {
 
     private int value;
 
-    public Card(String rank, String suit, int value){
-
-        rank = rank;
-
-        suit = suit;
-
-        value = value;
-
+    public Card(String c_suit, int c_value){
+        suit = c_suit;
+        value = c_value;
+        if (c_value > 10) {
+            if (c_value == 11) {
+                rank = "Jack";
+            }
+            else if (c_value == 12) {
+                rank = "Queen";
+            }
+            else if (c_value == 13) {
+                rank = "King";
+            }
+            else if (c_value == 14) {
+                rank = "Ace";
+            }
+        }
+        else {
+            rank = String.valueOf(c_value);
+        }
     }
 
     public String getRank(){
-
         return rank;
     }
 
     public String getSuit(){
-
         return suit;
     }
 
     public int getValue(){
-
         return value;
+    }
+
+    //@Override
+    public String toString (Card c) {
+        return "Card is " + c.rank + "of " + c.suit;
     }
 }
