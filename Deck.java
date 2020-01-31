49793fcbf0eada09da52812443c240dc78d4aa15
erinalara <48671321+erinalara@users.noreply.github.com>
2020-01-30 23:16:43 -0800

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -11,7 +11,7 @@ public class Deck {
         String card_suit = "";
         for (int i = 0; i < 4; i++) {
 
-            for (int j = 0; i < 13; j++) {
+            for (int j = 1; j < 15; j++) {
 
                 if (i == 0) {
                     card_suit = "Hearts";
@@ -33,40 +33,40 @@ public class Deck {
 
     public void shuffle() {
 
-        int max = deck.size();
+        int max = deck.size()-1;
         int min = 1;
 
-        for (int i = 0; i < deck.size(); i++) {
+        for (int i = 0; i < max; i++) {
 
-            int rd = (int)(( Math.random() * (max - min + 1)))  ;
-            System.out.println(rd); // random number
+            int rd = (int)(Math.random() * (max - min + 1)) + min;
 
             Card c_switch = deck.get(i);
             Card c_swap = deck.get(rd);
 
-        }
+            deck.remove(deck.get(i));
+            deck.add(i,c_swap);
+            deck.remove(deck.get(rd));
+            deck.add(rd, c_switch);
 
+        }
         for (int j = 0; j < 52; j++) {
-            System.out.println(deck.get(j));
+            System.out.println(deck.get(j).toString(deck.get(j)));
         }
-
 
     }
 
     public void deal(int players){
 
 
-
     }
 
-    public void plsy() {
+    public void play() {
 
 
     }
 
     @Override
     public String toString () {
-
         return "";
     }
 
