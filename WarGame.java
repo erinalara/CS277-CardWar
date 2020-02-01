fb9782e6365e6aed937781df6d8a0f20944e3025
erinalara <48671321+erinalara@users.noreply.github.com>
2020-02-01 14:31:14 -0800

    Update WarGame.java

--- WarGame.java
+++ WarGame.java
@@ -1,12 +1,35 @@
+import java.util.ArrayList;
+
 public class WarGame {
 
-  
-  
-  
-  public String toString() {
-    return ""
-      
-  }
-  
-  
+    private int wins;
+
+    public void play(int playerNum) {
+
+        Deck d = new Deck();
+        d.addCards();
+        d.shuffle();
+
+        ArrayList <ArrayList> players_decks = new ArrayList<>();
+        for (int i = 0; i < playerNum; i++) {
+            ArrayList player = d.deal(playerNum);
+            players_decks.add(player);
+        }
+
+
+
+
+    }
+
+    public void gameOver(){
+
+
+    }
+
+    public String toString() {
+        return "";
+
+    }
+
+
 }
