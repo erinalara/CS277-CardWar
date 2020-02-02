bd78f059ec8bd7ff7567f2a0cf1eeb212a27edac
erinalara <48671321+erinalara@users.noreply.github.com>
2020-02-01 17:58:43 -0800

    Update WarGame.java

--- WarGame.java
+++ WarGame.java
@@ -16,6 +16,21 @@ public class WarGame {
             players_decks.add(player);
         }
 
+        //Card max;
+        // ERIC IDK HOW TO FIX THIS 
+        // how to like idk uhhh remember the players' cards and compare
+        for (int j = 0; j < playerNum; j++) {
+            ArrayList <Card> player = players_decks.get(j);
+            Card p_card = player.remove(0);
+            System.out.println("Player " + (j+1) + "'s " + p_card.toString());
+        }
+
+        
+
+
+
+
+
 
 
 
